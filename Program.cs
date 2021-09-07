6b19a1668041cffd4cd3648d71b11afa492087c0
Panos Antoniadis <antoniadis.p1@gmail.com>
2021-09-07 16:21:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson;
+
+namespace Refactoring_and_Testing_06_09_21_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var res1 = Helper.CalculatePercentageGuardFailFast("150", "25");
+            Console.WriteLine(res1 + "%");
+        }
+    }
+}
